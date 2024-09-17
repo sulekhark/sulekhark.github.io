611f24153fb3fa3ddc7b0ae1114561571d242526
Sulekha Kulkarni <Sulekha.Kulkarni@microsoft.com>
2024-09-17 13:50:47 -0700

    Testing.

--- MM-Dev/MotionMark/tests/core/resources/multiply-variant.js
+++ MM-Dev/MotionMark/tests/core/resources/multiply-variant.js
@@ -137,7 +137,7 @@ var MultiplyVariantStage = Utilities.createSubclass(Stage,
 
     animate: function()
     {
-	console.log("SRK: HELLO 4 ");
+	console.log("SRK: HELLO 5 ");
         for (var i = 0; i < this._offsetIndex && i < 10; ++i) {
             var tile = this.tiles[i];
             const rotation = window.getComputedStyle(tile.element).getPropertyValue('transform');
